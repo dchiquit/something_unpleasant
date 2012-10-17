c41a871e442395f47c7a2fdd8c28420b7b5afe4b
daniel.chiquito@gmail.com <Daniel@Daniel-PC.(none)>
2012-10-16 22:31:46 -0400

    fixing it now?

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -3,36 +3,46 @@
 """
 
 class Node:
-    [] children
-    Node parent
-    "" value
-    "" type
+	[] children
+	Node parent
+	"" value
+	"" type
 
 class Parser:
-    {} class_variables
-    {} static_variables
-    {} function_arguments
-    {} function_locals
-    ^one of these would have type, index (number)
+	{} class_variables
+	{} static_variables
+	{} function_arguments
+	{} function_locals
+	^one of these would have type, index (number)
 
 """
 
 class Tokenizer:
-    
-    def __init__(self, file):
-        self.file = file
-        self.tokens = []
-		_preprocess()
+	
+	def __init__(self, file):
+		self.file = file
+		self.tokens = []
+		self._preprocess()
+		self.file = self.file.replace("\n","").replace("\t","").replace(" ","")
+		print(self.file)
 	
 	def _preprocess(self):
 		pass
 
-    def popToken(self):
-        self.tokens = [(type,value)] + self.tokens
-        return (type, value)
+	def _matchesToken(self, strang):
+		return self.file[:len(strang)] == strang
 
-    def pushToken(self):
-        self.file = self.tokens[0][1] + self.file
-        self.tokens = self.tokens[1:]
+	def popToken(self):
+		self.tokens = [(type,value)] + self.tokens
+		return (type, value)
 
+	def pushToken(self):
+		self.file = self.tokens[0][1] + self.file
+		self.tokens = self.tokens[1:]
 
+if __name__ == "__main__":
+	tokenizer = Tokenizer("class Yo{}")
+	print tokenizer.popToken()
+		
+		
+		
