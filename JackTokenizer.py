d4e55facebbecc0e49eee4bf99d324d98949a92b
daniel.chiquito@gmail.com <Daniel@Daniel-PC.(none)>
2012-10-16 21:58:56 -0400

    added a JackErrors.py file for containing errors.

--- JackTokenizer.py
+++ JackTokenizer.py
@@ -16,8 +16,6 @@ class Parser:
     ^one of these would have type, index (number)
 
 """
-keyword: ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this' , 'let', 'do', 'if', 'else', 'while', 'return']
-symbol: ['{' , '}' , '(' , ')' , '[' , ']' , '. ' , ',' , '; ' , '+' , '-' , '*' , '/' , '&' , ',' , '<' , '>' , '=' ,  '~' ]
 
 class Tokenizer:
     
