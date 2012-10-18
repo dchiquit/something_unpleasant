fb65ab1c0464c0362614b3249f460f2e32f862c7
zgao <mgao4793@gmail.com>
2012-10-18 17:29:49 -0400

    A few new constants

--- JackConstants.py
+++ JackConstants.py
@@ -1,4 +1,13 @@
-
-
 keyword = ['class', 'constructor', 'function', 'method', 'field', 'static', 'var', 'int', 'char', 'boolean', 'void', 'true', 'false', 'null', 'this' , 'let', 'do', 'if', 'else', 'while', 'return']
 symbol = ['{' , '}' , '(' , ')' , '[' , ']' , '.' , ',' , ';' , '+' , '-' , '*' , '/' , '&' , ',' , '<' , '>' , '=' ,  '~' ]
+
+termination_token = ("eof", None)
+
+IF_STATEMENT = 0
+LET_STATEMENT = 1
+WHILE_STATEMENT = 2
+DO_STATEMENT = 3
+RETURN_STATEMENT = 4
+METHOD = 5
+
+
