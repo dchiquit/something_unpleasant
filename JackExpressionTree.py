78b146a1ae023f99ce322411ad7747ebecbf5b9d
zgao <mgao4793@gmail.com>
2012-10-18 13:50:33 -0400

    Improvements to JackExpressionTree constructor

--- JackExpressionTree.py
+++ JackExpressionTree.py
@@ -2,15 +2,13 @@
 
 class JackExpressionTree:
 
-	#properties children, parent
-	# properties: {}
-	
-	def __init__(self, properties, value, parent):
-		self.properties = properties
-		self.parent = parent
-		self.children = []
-		self.parent._addChild(self)
-	
-	def _addChild(child):
-		self.children += [child]
-	
+    #properties children, parent
+    # properties: {}
+
+    def __init__(self, properties, parent, children = []):
+        self.properties, self.parent, self.children = properties, parent, children
+        self.parent._addChild(self)
+        c.parent = self for c in children
+
+    def _addChild(child):
+        self.children += [child]
